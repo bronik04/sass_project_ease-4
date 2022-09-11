cb8168de6e2045f2b3306541658c24083830eca2
Nikolay Brovkin <bronik004@gmail.com>
2022-09-11 14:58:56 +0300

    test commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,9 @@
 import './index.scss';
+
+const burgerButton = document.querySelector(".header__burger");
+const menu = document.querySelector(".menu");
+
+burgerButton.addEventListener("click", () => {
+  console.log('Click');
+  menu.classList.toggle('menu_active');
+});
