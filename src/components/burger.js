cbcad48d1ca6e677a5e6f00ced29b6777d7e5b4e
Nikolay Brovkin <bronik004@gmail.com>
2022-09-16 10:23:46 +0300

    feat: added hide burger and mobile PP

--- src/components/burger.js
+++ src/components/burger.js
@@ -1,9 +1,18 @@
-const burgerButton = document.querySelector(".header__burger");
-const menu = document.querySelector(".menu");
+const burgerButton = document.querySelector('.header__burger');
+const menu = document.querySelector('.menu');
 const logo = document.querySelector('.logo');
 
-burgerButton.addEventListener("click", () => {
+const toggleBurger = () => {
   burgerButton.classList.toggle('header__burger_active');
   menu.classList.toggle('menu_active');
-  // logo.classList.toggle('logo_hidden');
+};
+
+burgerButton.addEventListener('click', () => {
+  toggleBurger();
+});
+
+menu.addEventListener('click', evt => {
+  if (evt.target.classList.contains('navigation__link')) {
+    toggleBurger();
+  }
 });
